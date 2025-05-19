166e011c5e138e307c00ea7af41fae68e0613498
Naoto Takahashi <naoto0715charlie@gmail.com>
2025-05-19 10:51:33 +0900

    不要なファイル削除

--- Quiz_app/Quiz_app/Quiz_appApp.swift
+++ Quiz_app/Quiz_app/Quiz_appApp.swift
@@ -1,14 +1,14 @@
 //
-//  aaaaaa_appApp.swift
-//  aaaaaa_app
+//  Quiz_appApp.swift
+//  Quiz_app
 //
-//  Created by 高橋直斗 on 2025/05/15.
+//  Created by 高橋直斗 on 2025/05/18.
 //
 
 import SwiftUI
 
 @main
-struct aaaaaa_appApp: App {
+struct Quiz_appApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
